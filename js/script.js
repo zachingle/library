2cf4174c8a48da016f384052985dc9f368e4b401
ZachIngle <fzingle@internode.on.net>
2020-10-21 20:52:06 +1100

    Initial code for app. JQuery library added

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  let title = prompt("Title: ");
+  let author = prompt("Author: ");
+  let pages = prompt("Pages: ");
+  let read = prompt("Read: ");
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
